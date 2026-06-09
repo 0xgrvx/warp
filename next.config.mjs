8323fa5edb2363418b9d4b1bb6503939a28478b1
Gaurav Mandall <mandalgaurav550@gmail.com>
2026-06-10 03:33:08 +0530

    build: simplify next config for cloudflare pages build
    Co-authored-by: Gaurav <72154932+gauravmandall@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/warp',
+  output: 'export',
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
